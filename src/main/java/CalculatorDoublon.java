1f8be77a3f2a27567e7fd8b5c5a3ea98d1674737
kriss <kriss.clotilde@gmail.com>
2024-03-07 19:49:22 +0100

    deuxieme test if we have only a number in the list we retrun the number

--- src/main/java/CalculatorDoublon.java
+++ src/main/java/CalculatorDoublon.java
@@ -3,6 +3,9 @@ import java.util.Optional;
 
 public class CalculatorDoublon {
     public int calculDoublon(List<Integer> integers) {
+        if(integers.size()==1){
+            return integers.get(0);
+        }
         return 0;
     }
 }
--- src/test/java/ListDoublonTest.java
+++ src/test/java/ListDoublonTest.java
@@ -7,14 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ListDoublonTest {
 
     @Test
-    public void should_return_empty_array_when_no_data_are_setting() {
+    public void should_return_0_when_no_data_are_setting() {
         //Given
         ArrayList listNumbers= new ArrayList<>();
         //when
         int result = new CalculatorDoublon().calculDoublon(listNumbers);
         //Then
         assertEquals(0, result);
+    }
 
+    @Test
+    public void should_return_1_when_the_list_contaoins_only_1() {
+        //Given
+        ArrayList listNumbers= new ArrayList<>();
+        listNumbers.add(1);
+        //when
+        int result = new CalculatorDoublon().calculDoublon(listNumbers);
+        //Then
+        assertEquals(1, result);
     }
 
 
